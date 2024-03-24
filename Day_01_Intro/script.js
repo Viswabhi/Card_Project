0a1a4536a1ac368def3fae007859c45d27979c70
Viswabhi <avishek.viswakarma@gmail.com>
2024-03-24 23:30:28 +0530

    first commit

---
+++ Day_01_Intro/script.js
@@ -0,0 +1,34 @@
+// console.log("Hello World!!!");
+
+// console.log(1,2,3,4,5,6,7,8,9);
+
+// SubmitEvent()
+
+// let ab= "Avishek";
+// var sum = 80;
+// var sum = 90;
+// var sum = 900;
+// const yes = true;
+
+// let Ab= "Avishek";
+// console.log(ab, sum,sum, yes, Ab);
+
+// let a = 50;
+// let b = "Avishek Biswakarma";
+// let c = true;
+// let d = null;
+// let e = undefined;
+// let f = BigInt(235234624534575367735375);
+// let g = Symbol('Geekster!!!');
+
+// console.log(typeof a,typeof b,typeof c,typeof d,typeof e,typeof f,typeof g);
+
+let obj = {
+    name: "Avishek Biswakarma",
+    job: "SDE",
+    company: "Geekster",
+    marks: 77,
+    age: 24
+}
+obj.location = "Bangalore"
+console.log(obj);
